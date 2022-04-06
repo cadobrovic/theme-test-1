ed69439894c413bf3db74cd1fe1bead56f8362ec
Carl Dobrovic <cadobrovic@gmail.com>
2022-04-05 22:52:34 -0400

    Static uncomments for next config.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPlugins = require("next-compose-plugins")
 const reactSvg = require("next-react-svg")
 const path = require("path")
 
-const basePath = process.env.NODE_ENV === 'production' ? '/theme-test-1' : '';
+// const basePath = process.env.NODE_ENV === 'production' ? '/theme-test-1' : '';
 
 
 module.exports = withPlugins(
@@ -18,14 +18,14 @@ module.exports = withPlugins(
   {
     images: {
       deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-      // loader: "imgix", // Uncomment this line for STATIC EXPORT
-      // path: "", // Uncomment this line for STATIC EXPORT
+      loader: "imgix", // Uncomment this line for STATIC EXPORT
+      path: "", // Uncomment this line for STATIC EXPORT
     },
     env: {
       production_type: "static", // Change variable to "static" for STATIC EXPORT
     },
     trailingSlash: true, // Uncomment this line for STATIC EXPORT
-    basePath,
-    assetPrefix: `${basePath}/`
+    basePath: '/theme-test-1',
+    assetPrefix: '/theme-test-1/'
   }
 )
